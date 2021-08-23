27badbf1e2bbb65b89d9bb7f50b0608bd2ff3c4a
maithevien9 <maithevien9@gmail.com>
2021-08-23 09:17:17 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit and save to reload2.</p>
+        <p>Edit and save to reload23.</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           OK luon
         </a>
